82cd599111de01cbcc5b67e40d986ff8fd4cf10c
Yadhav Jayaraman <yjayaraman@pivotal.io>
2016-06-17 16:01:11 -0400

    modified test

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -5,22 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
 @WebAppConfiguration
-@RestController
 public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
 	
-	@RequestMapping("/")
-	public String greet() {
-		return "hello world";
-	}
 
 }
