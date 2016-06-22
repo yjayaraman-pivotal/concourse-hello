e2bfe75c2345cae0b3bc2b0577e74e435ad11817
Yadhav Jayaraman <yjayaraman@pivotal.io>
2016-06-22 12:20:07 -0400

    added integration

---
+++ concourse/tasks/prepare-integration.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+set -e -x
+
+inputDir=  outputDir=  versionFile=  manifest=  artifactId=  packaging=
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    -i | --input-dir )
+      inputDir=$2
+      shift
+      ;;
+    -o | --output-dir )
+      outputDir=$2
+      shift
+      ;;
+    -v | --version-file )
+      versionFile=$2
+      shift
+      ;;
+    -m | --manifest )
+      manifest=$2
+      shift
+      ;;
+    -a | --artifactId )
+      artifactId=$2
+      shift
+      ;;
+    -p | --packaging )
+      packaging=$2
+      shift
+      ;;
+    * )
+      echo "Unrecognized option: $1" 1>&2
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+error_and_exit() {
+  echo $1 >&2
+  exit 1
+}
+
+if [ ! -d "$inputDir" ]; then
+  error_and_exit "missing input directory: $inputDir"
+fi
+if [ ! -d "$outputDir" ]; then
+  error_and_exit "missing output directory: $outputDir"
+fi
+if [ ! -f "$versionFile" ]; then
+  error_and_exit "missing version file: $versionFile"
+fi
+if [ ! -f "$manifest" ]; then
+  error_and_exit "missing manifest file: $manifest"
+fi
+if [ -z "$artifactId" ]; then
+  error_and_exit "missing artifactId!"
+fi
+if [ -z "$packaging" ]; then
+  error_and_exit "missing packaging!"
+fi
+
+version=`cat $versionFile`
+artifactName="${artifactId}-${version}.${packaging}"
+
+echo $version
+echo $artifactName
+
