c11b39814a92a5263ebcc0877062ff82d6f229f9
Yadhav Jayaraman <yjayaraman@pivotal.io>
2016-06-23 22:53:36 -0400

    added manifest changes

--- concourse/tasks/prepare-integration.sh
+++ concourse/tasks/prepare-integration.sh
@@ -86,7 +86,7 @@ cp $manifest $outputManifest
 # the path in the manifest is always relative to the manifest itself
 sed -i -- "s|path: .*$|path: $artifactName|g" $outputManifest
 
-if [ ! -z "$hostname" ]; then
+if [ ! -z "$version" ]; then
   sed -i "s|host: .*$|&-$version|g" $outputManifest
 fi
 
